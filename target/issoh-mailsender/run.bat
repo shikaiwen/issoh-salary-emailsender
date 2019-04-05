162ac539b4d874a6d3c24807f829d985e6faf461
kevin <googlevsbing@126.com>
2019-04-05 19:32:22 +0900

    comm

--- target/issoh-mailsender/run.bat
+++
@@ -1,2 +0,0 @@
-D:\shikaiwen\fenghe\projects\kowas\trunk\issoh-mail-sender\target\issoh-mailsender\jre\bin\java.exe -jar ./mail-sender-2.0.jar
-pause
--- target/run.bat
+++
@@ -1,2 +0,0 @@
-D:\pleiades\java\8-x64\jre\bin\java.exe -jar ./mail-sender-2.0.jar
-pause
